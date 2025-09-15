e68c1b95a7620818c1c8befbc62991e6325ad5bc
neelzanwar26 <neelzanwar04@gmail.com>
2025-09-16 01:37:00 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
+
       "@": path.resolve(__dirname, "./src"),
     },
   },
